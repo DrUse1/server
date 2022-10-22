a30bc9df7a3689bcf402e10cdb97f7786a5a2a9b
BuildTools <unconfigured@null.spigotmc.org>
2022-10-23 00:32:53 +0200

    Revert "test"
    This reverts commit 26b39a152d6d4adb4cc4a6292881427c1c678e54.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,13 @@ import { staticConst, userInfo } from "../globalInfo";
 import styles from '../styles/header.module.scss'
 import { LogoFull } from "./elements";
 
+document.getElementsByTagName('html')[0].addEventListener('click', () => {
+    setTimeout(() => {
+        if (document.getElementsByClassName(styles.headerAccountDropdown).length === 0) return
+        document.getElementsByClassName(styles.headerAccountDropdown)[0].classList.remove(styles.active)
+    }, 1);
+})
+
 export default function Header() {
     const navigate = useNavigate()
     return (
@@ -27,7 +34,16 @@ export default function Header() {
                     </button>
                 </Show>
                 <div className={styles.headerAccountWrapper}>
-                    <button className={styles.headerAccountContent}>
+                    <button className={styles.headerAccountContent}
+                        onClick={() => {
+                            if (!document.getElementsByClassName(styles.headerAccountDropdown)[0].className.includes(styles.active)) {
+                                setTimeout(() => {
+                                    if (!document.getElementsByClassName(styles.headerAccountDropdown)[0].className.includes(styles.active)) {
+                                        document.getElementsByClassName(styles.headerAccountDropdown)[0].classList.add(styles.active)
+                                    }
+                                }, 2);
+                            }
+                        }}>
                         <div className={styles.profilePic}>
                             <span>{userInfo.prenom[0] + userInfo.nom[0]}</span>
                         </div>
